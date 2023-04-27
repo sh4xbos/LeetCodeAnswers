05867ba6b869181b81086a515a40f9ca42a47ebd
shakhbos <khamrayevsh12@gmail.com>
2023-04-28 01:23:45 +0500

    first commit

---
+++ src/test/java/com/example/leetcodeanswers/LeetCodeAnswersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcodeanswers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeAnswersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
